21ff25334c21171933456ea021ab7b345155e652
julkurz07 <kurzj062@gmail.com>
2026-08-18 11:03:22 +0200

    login basics

--- Mensa Projekt/Login.java
+++ Mensa Projekt/Login.java
@@ -9,6 +9,7 @@ public class Login extends JFrame {
   // Anfang Attribute
   private DatabaseConnector dbConnector;
 
+
   // Ende Attribute
   
   public Login() {
@@ -19,7 +20,33 @@ public class Login extends JFrame {
   }
   
   // Anfang Methoden
-  
+  public void login(int pID, String pPasswort) { 
+      dbConnector.executeStatement("SELECT Passwort FROM Nutzer WHERE uID LIKE '"+pID+"'");
+      QueryResult r = dbConnector.getCurrentQueryResult();
+      String passwort = r.getData()[0][0];
+      if(passwort.equals(pPasswort)) {
+          System.out.println("Anmeldedaten richtig!");
+          dbConnector.executeStatement("SELECT Rolle FROM Nutzer WHERE uID LIKE '"+pID+"'");
+          QueryResult s = dbConnector.getCurrentQueryResult();
+          String rolle = s.getData()[0][0];
+          if (rolle.equals("Schüler")) {
+              System.out.println("Schüler");
+              
+          }
+          else if (rolle.equals("Admin")) {
+              System.out.println("Admin");
+          }
+          else if (rolle.equals("Mensa")) {
+              System.out.println("Mensa");
+          }
+          else {
+              System.out.println("Du hast keine Berechtigung!");
+          }
+      }
+      else {
+          System.out.println("Anmeldedaten falsch!");
+      }
+  }
   public void dbVerbinden() {
     dbConnector = new DatabaseConnector("localhost", 3306, "Mensa", "root", "");
     String fehler = dbConnector.getErrorMessage();
@@ -29,7 +56,10 @@ public class Login extends JFrame {
       System.out.println("Fehlermeldung: " + fehler);
     }
   }
-   
+  
+  public Nutzer erstelleSchueler(int pID, String pVorname, String pNachname, String pPasswort) {
+      
+  }
   // Ende Methoden
   
   public static void main(String[] args) {
